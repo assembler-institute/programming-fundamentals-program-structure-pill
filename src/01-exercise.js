aa0084cafa23fadc24c55beb099c394cd48c51e8
Dani Lucaci <danilucaci@gmail.com>
2020-04-09 10:00:02 +0200

    add 01-exercise

--- src/01-exercise.js
+++ src/01-exercise.js
@@ -1,4 +1,21 @@
-function sum(a, b) {
-    return a + b;
+/**
+ * Create two variables with the name 'myvar'
+ *
+ * 1. the first variable name will have to have the first letter in upper case
+ * 2. the second variable name will have to have the first letter in lower case
+ *
+ * The variables should have any string as values.
+ * The variable values should be different.
+ */
+
+function variableNaming() {
+    // Write your code bellow this line
+
+    // Don’t change the code bellow this line
+    return {
+        myvar: myvar,
+        Myvar: Myvar,
+    };
 }
-module.exports = sum;
+
+module.exports = variableNaming;
--- src/__tests__/01-exercise.test.js
+++ src/__tests__/01-exercise.test.js
@@ -1,5 +1,24 @@
-const sum = require('./../01-exercise');
+const variableNaming = require('../01-exercise');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('01-exercise', () => {
+    test('creates a variable  with an uppercase first letter', () => {
+        expect(() => {
+            variableNaming();
+        }).not.toThrow();
+        expect(variableNaming().Myvar).toEqual(expect.any(String));
+    });
+
+    test('creates a variable with a lowercase first letter', () => {
+        expect(() => {
+            variableNaming();
+        }).not.toThrow();
+        expect(variableNaming().myvar).toEqual(expect.any(String));
+    });
+
+    test('variables have different values', () => {
+        expect(() => {
+            variableNaming();
+        }).not.toThrow();
+        expect(variableNaming().Myvar).not.toBe(variableNaming().myvar);
+    });
 });
