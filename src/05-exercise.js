52120d6a3e607050312f2e1702c5d64f090b5694
Dani Lucaci <danilucaci@gmail.com>
2020-04-14 10:11:13 +0200

    update exercise 5 instructions

--- src/05-exercise.js
+++ src/05-exercise.js
@@ -1,5 +1,7 @@
 /**
- * Write a function that returns the sum of 3 parameters 'a', 'b' and 'c'
+ * Update the function 'exercise05'
+ * to return the sum of the 3 parameters
+ * 'a', 'b' and 'c' that it receives
  */
 function exercise05(a, b, c) {
     // Write your solution bellow this line
